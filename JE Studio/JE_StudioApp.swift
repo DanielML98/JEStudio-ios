c17eea1cbf720a2cbb17015a9f43a074577215e4
Daniel Martinez <danielmn685@gmail.com>
2022-05-27 15:10:35 -0500

    Initial Commit

---
+++ JE Studio/JE_StudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  JE_StudioApp.swift
+//  JE Studio
+//
+//  Created by Daniel Martínez on 27/05/22.
+//
+
+import SwiftUI
+
+@main
+struct JE_StudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
