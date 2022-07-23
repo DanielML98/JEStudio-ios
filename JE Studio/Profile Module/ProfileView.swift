96561700782f514d1f75ef3fb8d72b419b12eae5
Daniel Martinez <danielmn685@gmail.com>
2022-07-22 23:45:08 -0500

    Added profile view

--- JE Studio/Profile Module/ProfileView.swift
+++ JE Studio/Profile Module/ProfileView.swift
@@ -9,16 +9,73 @@ import SwiftUI
 
 struct ProfileView: View {
   @Binding var isLoggedIn: Bool
-    var body: some View {
+  var body: some View {
+    VStack() {
+      Image("je-logo")
+        .resizable()
+        .scaledToFit()
+        .clipShape(Circle())
+        .aspectRatio(1/2, contentMode: .fit)
+        .foregroundColor(JEStudioColor.purple500)
+        .overlay(Circle().stroke(JEStudioColor.purple600, lineWidth: 5))
+      Text("Daniel Martinez")
+        .font(.jeHeader4)
+        .foregroundColor(JEStudioColor.purple500)
+      //Spacer()
+      VStack(alignment: .leading , spacing: 16) {
+        Text("Settings")
+          .foregroundColor(.white)
+          .font(.jeHeader2)
+        ScrollView {
+          settingsCell
+          Spacer()
+        }
+      }
+      .frame(maxWidth: .infinity)
+      .padding(8)
+      .background(JEStudioColor.purple500, ignoresSafeAreaEdges: .bottom)
+    }
+  }
+  
+  var settingsCell: some View {
+    VStack(alignment: .leading, spacing: 16) {
+      Button("My classes") {
+        print("Settings")
+      }
+      .frame(maxWidth: .infinity)
+      .background(.white)
+      .font(.jeHeader4)
+      .clipShape(Capsule())
+      Button("Settings") {
+        print("Settings")
+      }
+      .frame(maxWidth: .infinity)
+      .background(.white)
+      .font(.jeHeader4)
+      .clipShape(Capsule())
+      Button("Notifications") {
+        print("Settings")
+      }
+      .frame(maxWidth: .infinity)
+      .background(.white)
+      .font(.jeHeader4)
+      .clipShape(Capsule())
       Button("Log out") {
         AuthenticationManager.logOut()
         isLoggedIn.toggle()
       }
+      .frame(maxWidth: .infinity)
+      .background(.white)
+      .font(.jeHeader4)
+      .foregroundColor(.red)
+      .clipShape(Capsule())
+      .padding(.vertical)
     }
+  }
 }
 
 struct ProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-      ProfileView(isLoggedIn: .constant(true))
-    }
+  static var previews: some View {
+    ProfileView(isLoggedIn: .constant(true))
+  }
 }
---
+++ JE Studio/Profile Module/SettingsCellsview.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsCellsview.swift
+//  JE Studio
+//
+//  Created by Daniel Martínez on 21/07/22.
+//
+
+import SwiftUI
+
+struct SettingsCellsview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsCellsview_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsCellsview()
+    }
+}
